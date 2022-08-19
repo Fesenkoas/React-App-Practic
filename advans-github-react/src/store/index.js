8aaaf7aef5e4026b0bd1c28293365dc9b1ef124f
Aleksandr Fesenko <fesenkoas@gmail.com>
2022-08-19 10:48:40 +0300

    updata

--- advans-github-react/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { githubAPI } from "./github/github.api";
-
-export const store = configureStore({
-  reducer: {
-    [githubAPI.reducerPath]: githubAPI.reducer,
-  },
-});
---
+++ redux-toolkit/src/App.js
@@ -0,0 +1,11 @@
+import Counter from "./features/counter/Counter";
+
+function App() {
+  return (
+    <main className="App">
+      <h1>Redux Toolkit</h1>
+      <Counter />
+    </main>
+  );
+}
+export default App;
